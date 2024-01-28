caf08781d11ca218411f6396bb27134767848c53
jmaszybr <jmaszybr@gmail.com>
2024-01-28 22:48:06 +0100

    dodano komentarze

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ import base64
 import os
 import requests
 
-app = Flask(__name__)
+app = Flask(__name__) # Inicjalizacja instancji aplikacji Flask.
 
 def detect_faces(image):
+    # Wykrywa twarze na obrazie, konwertując go najpierw na odcienie szarości,
+    # a następnie używając kaskady Haar do identyfikacji twarzy.
+    # Zaznacza wykryte twarze prostokątem i zwraca zmodyfikowany obraz oraz liczbę wykrytych twarzy.
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
@@ -16,14 +19,22 @@ def detect_faces(image):
         cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
     return image, len(faces)
 
+
 def encode_image(image):
+    # Konwertuje obraz do JPEG, a następnie koduje go w Base64.
+    # Użyteczne do przesyłania obrazów w aplikacjach webowych, np. Flask.
     _, buffer = cv2.imencode('.jpg', image)
     encoded_image = base64.b64encode(buffer).decode('utf-8')
     return encoded_image
 
 @app.route('/', methods=['GET'])
 def index():
-    # Ładowanie i przetwarzanie przykładowego obrazu
+    # Obsługuje główną ścieżkę '/' aplikacji Flask.
+    # Wczytuje obraz 'images/test.png', wykrywa na nim twarze, a następnie koduje oryginalny
+    # i przetworzony obraz do formatu Base64.
+    # Zwraca te dane do szablonu 'index.html', wraz z liczbą wykrytych twarzy.
+    # Jeśli obraz nie istnieje, zwracane wartości są ustawione na None.
+
     example_image_path = 'images/test.png'
     if os.path.exists(example_image_path):
         example_image = cv2.imread(example_image_path)
@@ -40,9 +51,15 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    # Obsługuje ścieżkę '/upload' dla żądań POST.
+    # Przyjmuje przesłany plik obrazu, przetwarza go do formatu, który może być
+    # użyty przez OpenCV, a następnie wykrywa twarze na tym obrazie.
+    # Koduje oryginalny i przetworzony obraz do formatu Base64 i zwraca je do szablonu HTML,
+    # wraz z liczbą wykrytych twarzy.
+    # Jeśli plik nie został przesłany, zwraca szablon z komunikatem o błędzie.
+
     if 'file' not in request.files or request.files['file'].filename == '':
         return render_template('index.html', error="Brak obrazu do analizy")
-    file = request.files['file']
 
     file = request.files['file']
     in_memory_file = io.BytesIO()
